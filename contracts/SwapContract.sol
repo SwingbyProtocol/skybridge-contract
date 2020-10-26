3b985d32c8d5aae8404131390871d3eaeae0624a
syrohei <syrohei@gmail.com>
2020-10-26 16:54:40 +0900

    Rename contract name

--- contracts/SwapContract.sol
+++ contracts/SwapContract.sol
@@ -5,7 +5,7 @@ import "./Ownable.sol";
 import "./SafeMath.sol";
 
 // Gas cost = 553591 => 820814 gas for deploying (0.03875137 => 0.05745698 ETH at 70Gwei)
-contract MultiSendWallet is Ownable {
+contract SwapContract is Ownable {
     using SafeMath for uint256;
 
     constructor(address _owner) public {
