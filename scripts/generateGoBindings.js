3450f37269130c5bbda7762f56463b8459a8440f
JacobPlaster <plaster1995@gmail.com>
2020-12-11 16:50:42 +0000

    scripts/generateGoBindings: remove abosolute ref to abigen

--- scripts/generateGoBindings.js
+++ scripts/generateGoBindings.js
@@ -4,24 +4,12 @@ const { exec } = require("child_process")
 
 const abis = fs.readdirSync(path.resolve(__dirname, '../abigenBindings/abi'))
 
-try {
-  if (!fs.existsSync(`${process.env.GOROOT}/bin/abigen`)) {
-    console.error(`ERR: ${process.env.GOROOT}/bin/abigen not found`)
-    console.error(`Please install go-ethereum abigen to ${process.env.GOROOT} \n`)
-    console.log('cd $GOPATH/src/github.com/ethereum/go-ethereum')
-    console.log('go install ./cmd/abigen')
-    process.exit()
-  }
-} catch(err) {
-  process.exit()
-}
-
 abis.forEach((abi) => {
   const source = path.resolve(__dirname, '../abigenBindings/abi', abi)
   const goName = abi.replace(".abi", "")
   const destination = path.resolve(__dirname, '../abigo', goName)
 
-  const cmd = `${process.env.GOROOT}/bin/abigen --abi ${source} --pkg abigo --type ${goName} --out ${destination}.go`
+  const cmd = `abigen --abi ${source} --pkg abigo --type ${goName} --out ${destination}.go`
   exec(cmd, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
