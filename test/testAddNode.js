856e49af0922248da8d006da3281dcb52702a610
syrohei <syrohei@gmail.com>
2020-12-27 23:24:44 +0900

    fix: updated gas cost on tests

--- test/testAddNode.js
+++ test/testAddNode.js
@@ -20,7 +20,15 @@ contract('Test for churn and float', function (accounts) {
 
         this.WBTC_ADDR = this.wbtcTest.address
 
+        this.totalSwapped = new BN(0)
+
         this.swap = await SwapContract.new(this.lpToken.address, this.wbtcTest.address, 0);
+
+        this.redeemedFloatTxIds = [
+            "0x13e8785fe862e60f2caa4f838146ff9d4f4bd4a02dd6fb1f513b0a9be3452b62",
+            "0xce66450451e62b9b4c406d0a83b90a5036039673d2682d4ec292f375ae571382"
+        ]
+
 
         await this.lpToken.transferOwnership(this.swap.address)
     });
@@ -41,7 +49,7 @@ contract('Test for churn and float', function (accounts) {
     //     expect(bal).to.bignumber.equal('0')
     // })
 
-    it('add 100 nodes into swap contract', async function () {
+    it('add 100 nodes into swap contract then remove 12 nodes', async function () {
         let rewardAddressAndAmounts = []
         let isRemoved = []
         let churnedInCount = 25
@@ -81,6 +89,21 @@ contract('Test for churn and float', function (accounts) {
         let getNode2 = await this.swap.getActiveNodes()
         expect(getNode2.length).to.equal(100)
 
+
+        let floatAmountOfBTC = new BN(1).mul(new BN(10).pow(new BN(8)))
+
+        await this.wbtcTest.mint(this.swap.address, floatAmountOfBTC)
+
+        let swapTx = "0x" + web3.utils.padLeft(floatAmountOfBTC.toString('hex') + sender.slice(2), 64)
+        // fees are collected. (0.1 WBTC)
+        let rewardsAmount = "0x" + web3.utils.padLeft(new BN("1").mul(new BN(10).pow(new BN(5))).toString('hex'), 64)
+        await this.swap.multiTransferERC20TightlyPacked(this.WBTC_ADDR, [swapTx], this.totalSwapped, rewardsAmount, this.redeemedFloatTxIds)
+        // Second deposit tx
+
+        const distribute = await this.swap.distributeNodeRewards()
+        // Gas 3999911 gas
+        // console.log(distribute.receipt.cumulativeGasUsed)
+
         rewardAddressAndAmounts = []
         isRemoved = []
 
@@ -105,6 +128,4 @@ contract('Test for churn and float', function (accounts) {
         // console.log(tx3.receipt.cumulativeGasUsed)
         // Gas cost 51700 gas
     })
-
-
 })
