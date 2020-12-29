d51038f470b7815e582e9fae2f8e899918c60b8b
syrohei <syrohei@gmail.com>
2020-12-30 05:34:12 +0900

    fix: update LP token name and fixed tests

--- test/testLPToken.js
+++ test/testLPToken.js
@@ -6,7 +6,7 @@ const LPToken = artifacts.require('LPToken');
 
 contract('LPToken', function (accounts) {
     const [sender, receiver] = accounts
-    const name = 'Swingby BTC';
+    const name = "Swingby BTC LP Token";
     const symbol = 'sbBTC';
 
     beforeEach(async function () {
