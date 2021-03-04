9aadb637e76651bb297cec5207e73be06a2c91d4
syrohei <syrohei@gmail.com>
2021-03-05 07:09:45 +0900

    fix/tests: added some script to test in testnet for bsc network

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,21 +1,24 @@
 const SwapContract = artifacts.require("SwapContract");
 const LPToken = artifacts.require("LPToken");
 
-let WBTC_ADDR = "0x449268b65BAf7251f83fd0a4b182DbC4C20985Fd"
+let BTCT_ADDR = "0x449268b65BAf7251f83fd0a4b182DbC4C20985Fd"
 
 module.exports = async function (deployer, net) {
   if (net == "development") {
     return
   }
   if (net == "goerli") {
-    WBTC_ADDR = "0xEb47a21C1fC00D1E863019906df1771b80DBE182"
+    BTCT_ADDR = "0xEb47a21C1fC00D1E863019906df1771b80DBE182"
   }
   if (net == "mainnet") {
-    WBTC_ADDR = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
+    BTCT_ADDR = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
+  }
+  if (net == "bsc_testnet") {
+    BTCT_ADDR = "0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8"
   }
   await deployer.deploy(LPToken)
   const lpToken = await LPToken.deployed()
-  await deployer.deploy(SwapContract, lpToken.address, WBTC_ADDR, 0);
+  await deployer.deploy(SwapContract, lpToken.address, BTCT_ADDR, 0);
   const swapContract = await SwapContract.deployed()
   await lpToken.transferOwnership(swapContract.address)
 };
--- truffle-config.js
+++ truffle-config.js
@@ -79,7 +79,7 @@ module.exports = {
     bsc_testnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      gas: 7500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
