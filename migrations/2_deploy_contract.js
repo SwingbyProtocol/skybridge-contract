44a588832f811e8b5e82683cfc187b37d332ae63
syrohei <syrohei@gmail.com>
2021-03-06 15:11:43 +0900

    fix/contract: modified contract abi

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,12 +1,12 @@
 const SwapContract = artifacts.require("SwapContract");
 const LPToken = artifacts.require("LPToken");
-
-let BTCT_ADDR = "0x449268b65BAf7251f83fd0a4b182DbC4C20985Fd"
+const TOKEN_DECIMALS = process.env.TOKEN_DECIMALS || 18
 
 module.exports = async function (deployer, net) {
   if (net == "development") {
     return
   }
+  let BTCT_ADDR = "0x449268b65BAf7251f83fd0a4b182DbC4C20985Fd"
   if (net == "goerli") {
     BTCT_ADDR = "0xEb47a21C1fC00D1E863019906df1771b80DBE182"
   }
@@ -16,7 +16,7 @@ module.exports = async function (deployer, net) {
   if (net == "bsc_testnet") {
     BTCT_ADDR = "0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8"
   }
-  await deployer.deploy(LPToken, 18)
+  await deployer.deploy(LPToken, TOKEN_DECIMALS)
   const lpToken = await LPToken.deployed()
   await deployer.deploy(SwapContract, lpToken.address, BTCT_ADDR, 0);
   const swapContract = await SwapContract.deployed()
