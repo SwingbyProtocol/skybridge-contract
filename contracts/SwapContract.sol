494fe6ddc97b17894f3a2632b48ce0d3b25e0005
Jake <jabot999@gmail.com>
2021-12-20 14:54:02 -0600

    Deployed with change for min sats for Flow 2

--- contracts/SwapContract.sol
+++ contracts/SwapContract.sol
@@ -10,14 +10,15 @@ import "./interfaces/ITokenTransferProxy.sol";
 import "./interfaces/IParaswap.sol";
 import "./interfaces/lib/Utils.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./interfaces/lib/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./LPToken.sol";
 
 //import "hardhat/console.sol"; //console.log()
 
-//import "./interfaces/lib/SafeERC20.sol";
+
 
 
 
