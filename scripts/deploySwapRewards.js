ca7025666a6f0dae38109bc636865e5886d050f9
syrohei <syrohei@gmail.com>
2022-03-29 18:48:51 +0100

    feat: deployed all contracts

---
+++ scripts/deploySwapRewards.js
@@ -0,0 +1,28 @@
+const { ethers } = require("hardhat");
+
+/*********************************************************************
+ * 
+ * Boiler Plate script to deploy
+ * 
+ * July 28, 2021 
+ * 
+ * Deploy with:
+ * npx hardhat run ./scripts/deploy.js --network development || npx hardhat run ./scripts/deploy.js --network hardhat
+ * There are still some errors with the above
+ */
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const SWINGBY = "0x8287C7b963b405b7B8D467DB9d79eEC40625b13A"
+    const SwapRewardsFactory = await ethers.getContractFactory("SwapRewards");
+    const swapRewards = await SwapRewardsFactory.deploy(deployer.address, SWINGBY, 358520);
+
+    await swapRewards.deployed();
+    console.log("SwapRewards address:", swapRewards.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
