6aa7f6317abdb67b1ea76379b85f644cef00f205
syrohei <syrohei@gmail.com>
2023-05-31 02:20:58 +0900

    feat: update solc

--- contracts/SwapRewards.sol
+++ contracts/SwapRewards.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.6.0 <=0.8.19;
+pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
